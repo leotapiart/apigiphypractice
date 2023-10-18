a230456587e26b83fa99a5cba88e0b34e36e8300
Leonel <84729652+leotapiart@users.noreply.github.com>
2023-10-18 18:02:08 +0000

    Update 6 files

--- src/components/Gif/index.jsx
+++ src/components/Gif/index.jsx
@@ -5,7 +5,6 @@ const Gif = ({ title, url, id }) => {
   return (
     <div className="Gif">
       <Link to={`/gif/${id}`} className="Gif-link">
-        <h4>{title}</h4>
         <img loading="lazy" alt={title} src={url} />
       </Link>
     </div>
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const apiKEY = "U0N1cwjKdzCMYE19ofV99xlRev9CSG9S";
 
 const dataFetch = async ({ keyword = "gatos" } = {}) => {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKEY}&q=${keyword}&limit=20&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
   try {
     const resp = await fetch(apiURL);
